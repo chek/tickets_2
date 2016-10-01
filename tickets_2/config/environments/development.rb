4cdc77c631ed7cc5d400c07c4b3131ca16aa26f8
Israel Gavriel <israel@commun.it>
2016-10-01 19:14:25 +0300

    check logout, sass syntax

--- tickets_2/config/environments/development.rb
+++ tickets_2/config/environments/development.rb
@@ -53,4 +53,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.sass.preferred_syntax = :sass
 end
