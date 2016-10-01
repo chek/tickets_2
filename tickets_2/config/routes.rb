6ac5f22ed5e1bb0ff216e2a4429b780c4a91f4e4
Israel Gavriel <israel@commun.it>
2016-10-01 20:04:11 +0300

    sign in user ajax

--- tickets_2/config/routes.rb
+++ tickets_2/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :tickets
+  resources :users
+  post 'users/sign_in_ajax', to: 'users#sign_in_ajax'
   devise_for :users
   root to: "tickets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
