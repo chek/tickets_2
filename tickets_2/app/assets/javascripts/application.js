ecb729f835c3b964841612f8d5d76be8a12d6e69
Israel Gavriel <israel@commun.it>
2016-10-01 20:16:48 +0300

    add backbone

--- tickets_2/app/assets/javascripts/application.js
+++ tickets_2/app/assets/javascripts/application.js
@@ -15,4 +15,12 @@
 //= require turbolinks
 //= require tether
 //= require bootstrap
+//= require underscore
+//= require backbone
+//= require tickets2
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
---
+++ tickets_2/app/assets/javascripts/tickets2.js.coffee
@@ -0,0 +1,10 @@
+window.Tickets2 =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  initialize: ->
+    #alert 'Hello from Backbone!'
+
+$(document).ready ->
+  Tickets2.initialize()
