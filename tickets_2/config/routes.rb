c5a04d4589db51700e1d1e03d860b8704f1fbce7
Israel Gavriel <israel@commun.it>
2016-10-01 18:53:01 +0300

    default route tickets

--- tickets_2/config/routes.rb
+++ tickets_2/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :tickets
   devise_for :users
+  root to: "tickets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
