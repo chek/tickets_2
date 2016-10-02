abaeb6a090fd2f796dab0a9ce3474e3ad314d779
Israel Gavriel <israel@commun.it>
2016-10-02 07:32:57 +0300

    home controller

--- tickets_2/config/routes.rb
+++ tickets_2/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :tickets
   resources :users
   post 'users/sign_in_ajax', to: 'users#sign_in_ajax'
   post 'users/sign_out_ajax', to: 'users#sign_out_ajax'
   post 'users/sign_up_ajax', to: 'users#sign_up_ajax'
   devise_for :users
-  root to: "tickets#index"
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
